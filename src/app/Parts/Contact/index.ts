e9b419c2864edc08e00c8ae8799da8b8120cb34a
Damian Mostert <damianmostert86@gmail.com>
2023-07-22 15:32:24 +0200

    Add files via upload

---
+++ src/app/Parts/Contact/index.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  standalone:true,
+  imports: [],
+  selector: 'app-contact',
+  templateUrl:"./contact.html",
+  styleUrls: ['./contact.css'],
+})
+export default class Contact {
+
+}
